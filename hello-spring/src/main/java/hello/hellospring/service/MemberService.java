331bcbbab4433a5fd4da8dec95e198c2d40965ea
James park <dayung0425@gmail.com>
2023-03-02 17:24:11 +0900

    스프링 입문1회독

--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -23,10 +23,9 @@ public class MemberService {
      * 회원가입
      */
     public Long join(Member member) {
-        validateDuplicateMember(member); //중복 회원 검증
-        memberRepository.save(member);
-        return member.getId();
-    }
+        long start = System.currentTimeMillis();}
+
+
     private void validateDuplicateMember(Member member) {
         memberRepository.findByName(member.getName())
                 .ifPresent(m -> {
--- hello-spring/src/main/java/hello/hellospring/service/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/service/SpringConfig.java
@@ -1,5 +1,6 @@
 package hello.hellospring;
 //import hello.hellospring.repository.JdbcMemberRepository;
+import hello.hellospring.aop.TimeTraceAop;
 import hello.hellospring.repository.JdbcTemplateMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRespository;
@@ -19,6 +20,13 @@ public class SpringConfig {
     public MemberService memberService() {
         return new MemberService(memberRepository());
     }
+
+    @Bean
+    public TimeTraceAop timeTraceAop(){
+        return new TimeTraceAop();
+    }
+
+
 //    @Bean
 //    public MemberRepository memberRepository() {
 // return new MemoryMemberRepository();
